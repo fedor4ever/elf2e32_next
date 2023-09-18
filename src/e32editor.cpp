0eaef01c237fe532d2c706a33b6557012d000aee
Fiodar <fedor4ever@users.noreply.github.com>
2023-09-18 15:14:38 +0300

    Implement generator checksums for the E32Image sections

--- src/e32editor.cpp
+++ src/e32editor.cpp
@@ -1,77 +1,133 @@
-#include "e32editor.h"
+// Copyright (c) 2023 Strizhniou Fiodar
+// All rights reserved.
+// This component and the accompanying materials are made available
+// under the terms of "Eclipse Public License v1.0"
+// which accompanies this distribution, and is available
+// at the URL "http://www.eclipse.org/legal/epl-v10.html".
+//
+// Initial Contributors:
+// Strizhniou Fiodar - initial contribution.
+//
+// Contributors:
+//
+// Description:
+// Generate checksums for the E32Image sections
+//
+//
 
-E32Editor::E32Editor(const E32Parser* file)
+#include "e32common.h"
+#include "e32editor.h"
+#include "e32parser.h"
+#include "e32importsprocessor.hpp"
+
+E32Editor::E32Editor(const E32Parser* const file): iFile(file)
 {
     //ctor
 }
 
+void E32Editor::ConstructL()
+{
+    std::streamoff tmp = iFile->GetFileSize();
+    char* buf = new char[tmp]();
+    memcpy(buf, iFile->GetBufferedImage(), tmp);
+    iFile = new E32Parser(buf, tmp);
+    iHeader = iFile->GetFileLayout();
+    iE32File = iFile->GetBufferedImage();
+}
+
+E32Editor* E32Editor::NewL(const E32Parser* const file)
+{
+    E32Editor* self = new E32Editor(file);
+    self->ConstructL();
+    return self;
+}
+
 E32Editor::~E32Editor()
 {
+    delete iFile;
     delete[] iE32File;
 }
 
-char* E32Editor::Duplicate()
+void E32Editor::SetE32Time(uint32_t timeLo, uint32_t timeHi)
 {
-
-}
-
-uint32_t E32Editor::DataRelocs() const
-{
-
-}
-
-uint32_t E32Editor::CodeRelocs() const
-{
-
-}
-
-uint32_t E32Editor::Imports() const
-{
-
-}
-
-uint32_t E32Editor::Symlook() const
-{
-
-}
-
-uint32_t E32Editor::Exports() const
-{
-
-}
-
-uint32_t E32Editor::Data() const
-{
-
-}
-
-uint32_t E32Editor::Code() const
-{
-
-}
-
-uint32_t E32Editor::ExportBitMap() const
-{
-
-}
-
-uint32_t E32Editor::Header() const
-{
-
+    iHeader->iTimeLo = timeLo;
+    iHeader->iTimeHi = timeHi;
 }
 
 uint32_t E32Editor::FullImage() const
 {
+    return Crc32(iFile->GetBufferedImage(), iFile->GetFileSize());
+}
 
+uint32_t E32Editor::Header() const
+{
+    return Crc32(iFile->GetBufferedImage(), sizeof(E32ImageHeader) + sizeof(E32ImageHeaderJ) + sizeof(E32ImageHeaderV));
+}
+
+uint32_t E32Editor::ExportBitMap() const
+{
+    const E32ImageHeaderV* h = iFile->GetE32HdrV();
+    if(h->iExportDescSize == 0)
+        return -1;
+    return Crc32(h->iExportDesc, h->iExportDescSize);
+}
+
+uint32_t E32Editor::Code() const
+{
+    return Crc32(iFile->GetBufferedImage() + iHeader->iCodeOffset, iHeader->iCodeSize);
+}
+
+uint32_t E32Editor::Data() const
+{
+    if(iHeader->iDataSize == 0)
+        return -1;
+    return Crc32(iFile->GetBufferedImage() + iHeader->iDataOffset, iHeader->iDataSize);
+}
+
+uint32_t E32Editor::Exports() const
+{
+    if(iHeader->iExportDirOffset == 0)
+        return -1;
+    return Crc32(iFile->GetBufferedImage() + iHeader->iExportDirOffset, iHeader->iExportDirCount * 4);
+}
+
+uint32_t E32Editor::Symlook() const
+{
+    if(!(iHeader->iFlags & KImageNmdExpData))
+        return -1;
+    const E32EpocExpSymInfoHdr* h = iFile->GetEpocExpSymInfoHdr();
+    return Crc32(h, h->iSize);
+}
+
+uint32_t E32Editor::Imports() const
+{
+    const E32ImportSection* h = iFile->GetImportSection();
+    return Crc32(h, h->iSize);
+}
+
+uint32_t E32Editor::CodeRelocs() const
+{
+    uint32_t length = iFile->GetFileSize() - iHeader->iCodeRelocOffset;
+    if(iHeader->iDataRelocOffset > 0)
+        length = iHeader->iDataRelocOffset - iHeader->iCodeRelocOffset;
+    return Crc32(iFile->GetRelocSection(iHeader->iCodeRelocOffset), length);
+}
+
+uint32_t E32Editor::DataRelocs() const
+{
+    if(iHeader->iDataRelocOffset == 0)
+        return -1;
+    uint32_t length = iFile->GetFileSize() - iHeader->iDataRelocOffset;
+    return Crc32(iFile->GetRelocSection(iHeader->iDataRelocOffset), length);
 }
 
 uint32_t E32Editor::TimeHi() const
 {
-
+    return iHeader->iTimeHi;
 }
 
 uint32_t E32Editor::TimeLo() const
 {
-
+    return iHeader->iTimeLo;
 }
 
--- src/e32editor.h
+++ src/e32editor.h
@@ -1,15 +1,36 @@
+// Copyright (c) 2023 Strizhniou Fiodar
+// All rights reserved.
+// This component and the accompanying materials are made available
+// under the terms of "Eclipse Public License v1.0"
+// which accompanies this distribution, and is available
+// at the URL "http://www.eclipse.org/legal/epl-v10.html".
+//
+// Initial Contributors:
+// Strizhniou Fiodar - initial contribution.
+//
+// Contributors:
+//
+// Description:
+// Generate checksums for the E32Image sections
+//
+//
+
 #ifndef E32EDITOR_H
 #define E32EDITOR_H
 
 #include <stdint.h>
 
 class E32Parser;
+class E32ImageHeader;
 
 class E32Editor
 {
+        E32Editor(const E32Parser* const file);
+        void ConstructL();
     public:
-        E32Editor(const E32Parser* file);
+        static E32Editor* NewL(const E32Parser* const file);
         ~E32Editor();
+        void SetE32Time(uint32_t timeLo, uint32_t timeHi);
     public: //return E32Image header fields
         uint32_t TimeLo() const;
         uint32_t TimeHi() const;
@@ -25,9 +46,8 @@ class E32Editor
         uint32_t CodeRelocs() const;
         uint32_t DataRelocs() const;
     private:
-        char* Duplicate();
-    private:
-        E32Parser* iFile = nullptr;
+        E32ImageHeader* iHeader = nullptr;
+        const E32Parser* iFile = nullptr;
         const char* iE32File = nullptr;
 };
 

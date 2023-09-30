f0a846f038df534009c711932047a2cf95693f22
Fiodar <fedor4ever@users.noreply.github.com>
2023-10-01 02:35:09 +0300

    ResetInvalidLINKAS(): fix warning: suggest parentheses around '&&' within '||'

--- src/artifactbuilder.cpp
+++ src/artifactbuilder.cpp
@@ -250,7 +250,7 @@ void ResetInvalidLINKAS(Args* arg)
     size_t lst = arg->iLinkas.find_first_of("}");
     if((first == std::string::npos) || (last == std::string::npos) || (last < first) ||
        (fst == std::string::npos) || (lst == std::string::npos) || (lst < fst) ||
-       ((last - first) != 9) && ((lst - fst) != 9))
+       ( ((last - first) != 9) && ((lst - fst) != 9)) )
     {
         ReportWarning(ErrorCodes::ZEROBUFFER, "Illformed option: " + arg->iLinkas + "\n");
         ReportWarning(ErrorCodes::ZEROBUFFER, "Example: --linkas=foo{000a0000}[10011237].dll\n");

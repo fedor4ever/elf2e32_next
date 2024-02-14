749d80f717c7f98b1d966a1c5ef1ded24a4b15dd
Fiodar <fedor4ever@users.noreply.github.com>
2024-02-14 11:52:52 +0300

    Fix: use data from non-existed file

--- src/e32crcprocessor.cpp
+++ src/e32crcprocessor.cpp
@@ -314,6 +314,8 @@ E32CRCProcessor::~E32CRCProcessor()
 
 void E32CRCProcessor::FixE32Hdr()
 {
+    if(iFileIn.empty())
+        return;
 // For example in manual E32Image builds comression maybe off for easy hex view.
 // This change header and even entire file checksums and test failed.
 #ifndef SET_COMPILETIME_LOAD_EXISTED_FILECRC
